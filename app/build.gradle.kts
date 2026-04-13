7c87d223dd708391a2f943186010f6a11fc33f8e
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2026-04-13 13:17:09 +0300

    bump app version to 0.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.monogram"
         minSdk = 25
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
     }
 
     splits {
